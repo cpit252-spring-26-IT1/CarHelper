0d29d5ba15c868242edc3d00f867397834f14198
abmoqbil <amoqbil@stu.kau.edu.sa>
2026-04-09 18:34:20 +0300

    Add files via upload

---
+++ CarHelper/src/main/java/com/carhelper/CarHelperApplication.java
@@ -0,0 +1,13 @@
+package com.carhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
+
+@SpringBootApplication
+public class CarHelperApplication {
+
+	public static void main(String[] args) {
+
+		}
+	}
---
+++ CarHelper/src/test/java/com/carhelper/CarHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
