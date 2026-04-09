93d14fbb2345cb87e5fc9398bdabadc436643c07
abmoqbil <amoqbil@stu.kau.edu.sa>
2026-04-09 20:20:28 +0300

    Create User class #6

--- CarHelper/src/main/java/com/carhelper/user/UserAuthManager.java
+++ CarHelper/src/main/java/com/carhelper/user/UserAuthManager.java
@@ -1,4 +1,20 @@
 package com.carhelper.user;
 
 public class UserAuthManager {
+
+    private static UserAuthManager instance;
+
+    private UserAuthManager() {}
+
+    public static synchronized UserAuthManager getInstance() {
+        if (instance == null) {
+            instance = new UserAuthManager();
+        }
+        return instance;
+    }
+
+    public boolean loginUser(String email, String password) {
+        System.out.println("Login user: " + email);
+        return true;
+    }
 }
